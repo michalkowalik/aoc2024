12614e9d0bfd71cb949f9fe328e389afb6d7c6cf
Michal Kowalik <mkowalik@curriedpython.eu>
2025-01-03 17:17:52 +0100

    Day9, part 1

--- day9.py
+++ day9.py
@@ -1,14 +1,58 @@
 #!/usr/bin/env python
 
-def solve_part1():
-    pass
+def build_disk_map(data: str):
+    index = 0
+    file_index = 0
+    disk_map = []
 
-def solve_part2():
-    pass
+    for char in data:
+        if index % 2 == 0:
+            disk_map.extend([file_index]*int(char))
+            file_index += 1
+        else:
+            disk_map.extend([None]*int(char))
+        index += 1
+
+    return disk_map
+
+def checksum(disk_map):
+    sum = 0
+    for i in range(0, len(disk_map)):
+        if disk_map[i] is None:
+            continue
+        sum += disk_map[i]*i
+    return sum
+
+def solve_part1(disk_map):
+    file_blocks = len([x for x in disk_map if x is not None])
+    empty_blocks = len([x for x in disk_map if x is  None])
+    print (f"got {empty_blocks} empty blocks")
+
+    b_cursor = len(disk_map)-1
+    f_cursor = 0
+    while empty_blocks > 0:
+        if disk_map[f_cursor] is None:
+            while disk_map[b_cursor] is None:
+                b_cursor -= 1
+            disk_map[f_cursor] = disk_map[b_cursor]
+            empty_blocks -= 1
+            f_cursor += 1
+            b_cursor -= 1
+        else:
+            f_cursor += 1
+    print(f'checksum: {checksum(disk_map[:file_blocks])}')
+
+def solve_part2(disk_map):
+
+    print(f'checksum: {checksum(disk_map)}')
 
 def main():
-    print(solve_part1())
-    print(solve_part2())
+    with open('inputs/day9-test.txt') as f:
+        data = f.readline().strip()
+    disk_map = build_disk_map(data)
+
+    solve_part1(disk_map)
+    solve_part2(disk_map)
 
 if __name__ == '__main__':
-    main()
+        main()
