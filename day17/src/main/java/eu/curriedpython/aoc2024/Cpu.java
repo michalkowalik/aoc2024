2234888a8b347f29785b432a5472a4a0feebb2f7
Michal Kowalik <mkowalik@curriedpython.eu>
2025-01-30 23:18:21 +0100

    DAY17: example works, the program does not

--- day17/src/main/java/eu/curriedpython/aoc2024/Cpu.java
+++ day17/src/main/java/eu/curriedpython/aoc2024/Cpu.java
@@ -3,29 +3,36 @@ package eu.curriedpython.aoc2024;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 public class Cpu {
-    private Integer RegA;
-    private Integer RegB;
-    private Integer RegC;
+    private Integer regA;
+    private Integer regB;
+    private Integer regC;
+
+    List<Integer> output = new ArrayList<>();
 
     private int pc; // program counter
     private List<Integer> program;
 
     public Cpu(List<Integer> program, int regA, int regB, int regC) {
         this.program = program;
-        this.RegA = regA;
-        this.RegB = regB;
-        this.RegC = regC;
+        this.regA = regA;
+        this.regB = regB;
+        this.regC = regC;
     }
 
     public void run() {
         while (pc < program.size()) {
             step();
         }
+        System.out.println(output.stream().
+                map(Object::toString).
+                collect(Collectors.joining(",")));
     }
 
     private int fetch() {
@@ -51,30 +58,56 @@ public class Cpu {
         }
     }
 
+    private int comboOperand(int op) {
+        return switch (op & 7) {
+            case 0, 1, 2, 3 -> op;
+            case 4 -> regA;
+            case 5 -> regB;
+            case 6 -> regC;
+            default -> throw new IndexOutOfBoundsException();
+        };
+    }
+
     // opcodes
     private void adv(int operand) {
-
+        double nom = (double) regA;
+        double denom = (double) (2 << (comboOperand(operand)-1));
+        regA = (int) Math.floor(nom / denom);
     }
 
     private void bxl(int operand) {
+        regB = regB ^ (operand & 7);
     }
 
     private void bst(int operand) {
+        regB = comboOperand(operand) & 7;
     }
 
     private void jnz(int operand) {
+        if (regA == 0) {
+            return;
+        }
+        pc = operand;
     }
 
     private void bxc(int operand) {
+        regB = regB ^ regC;
     }
 
     private void out(int operand) {
+        output.add(comboOperand(operand) & 7);
     }
 
     private void bdv(int operand) {
+        double nom = (double) regA;
+        double denom = (double) (2 << comboOperand(operand)-1);
+        regB = (int) Math.floor(nom / denom);
     }
 
     private void cdv(int operand) {
+        double nom = (double) regA;
+        double denom = (double) (2 << comboOperand(operand)-1);
+        regC = (int) Math.floor(nom / denom);
     }
 
 }
